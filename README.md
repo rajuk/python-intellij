a009f6abe4c388c4bacea58f5ef61b01bb2cbc31
Raju Krishnamurthy <rkrishnamurthy@videoamp.com>
2024-10-01 16:18:39 -0700

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Minimal repo to test intellij under bazel
+
+Debugging with Intellij work if using unittest.main() but not with pytest.main()
--- example/pytest_test.py
+++ example/pytest_test.py
@@ -1,10 +1,8 @@
-import os
 import pytest
 import sys
 
 def test_foo():
-    assert 1
+    assert 0
 
 if __name__ == "__main__":
-    args = os.environ.get("_PYTEST_ARGS_", "").split("\n")
     sys.exit(pytest.main())
